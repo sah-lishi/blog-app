df49de845851fb7e50d86f31834d226475921178
sah-lishi <lishisaah@gmail.com>
2026-02-23 22:43:51 +0530

    made getAllBlogs route public

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -4,10 +4,9 @@ import upload from "../middlewares/multer.middleware.js"
 import { createBlog, deleteBlog, getAllBlogs, getBlogById, updateBlog } from "../controllers/blog.controller.js"
 
 const router = express.Router()
+router.route("/").get(getAllBlogs)
 router.use(jwtVerify)
-router.route("/")
-    .get(getAllBlogs)
-    .post(upload.single("blogCoverImage"), createBlog)
+router.route("/").post(upload.single("blogCoverImage"), createBlog)
 
 router.route("/:blog_id")
     .delete(deleteBlog)
